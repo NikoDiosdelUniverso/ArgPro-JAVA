6e8055fdf9f444b81aebf54bad65a65edd0a30cc
DESKTOP-JND0JDS\ROG BY NG <lailamed99@gmail.com>
2023-02-24 02:30:19 -0300

    primer commit

---
+++ Clase 1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int inicio = 10;
+        int fin = 20;
+        boolean par = false;
+
+        while(inicio <= fin){
+            if (par) {
+                if (inicio%2==0) {
+                    System.out.println(inicio);
+                }
+            } else {
+                if (inicio%2!=0) {
+                    System.out.println(inicio);
+                }
+            }
+            inicio=inicio+1;
+        }
+    }
+}
